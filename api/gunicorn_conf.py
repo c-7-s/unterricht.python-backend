6fe8f67187bb903fc8e360cffc59285258a971d1
CTS <cts_anubis@gmx.de>
2024-01-29 22:27:26 +0100

    [CS] worked on interface

--- api/gunicorn_conf.py
+++ api/gunicorn_conf.py
@@ -1,10 +1,8 @@
-import multiprocessing
-
 max_requests = 1000
 max_requests_jitter = 50
 timeout = 120
 log_file = "-"
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 3
 bind = '0.0.0.0:8811'
 worker_class = 'gevent'
 loglevel = 'debug'
